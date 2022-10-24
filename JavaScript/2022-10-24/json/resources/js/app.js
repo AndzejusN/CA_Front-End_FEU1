2e1b54fd5a2742b0e48e3649b07b64b37c6d2818
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-10-24 13:48:51 +0300

    2022-10-24/2 CA Frontend

--- JavaScript/2022-10-24/json/resources/js/app.js
+++ JavaScript/2022-10-24/json/resources/js/app.js
@@ -27,22 +27,42 @@ function createDom(movies) {
         ul.appendChild(liShortInfo);
 
         let liGenre = document.createElement('li');
-
-        let output = movie.genre.toString();
-        let result = output.split(',').join(', ');
-
-        liGenre.textContent = 'Genre: ' + result;
+        let outputGenre = movie.genre.toString().split(',').join(', ');
+        liGenre.textContent = 'Genre: ' + outputGenre;
         ul.appendChild(liGenre);
+
+        let liRating = document.createElement('li');
+        liRating.textContent = `Rating score: ${movie.rating.ratingScore}; Rating voters: ${movie.rating.numberOfVoters}`;
+        ul.appendChild(liRating);
+
+        let directors = [];
+
+        movie.director.map(director => {
+            directors.push([director.firstName, director.secondName + ';']);
+        })
+
+        let liDirector = document.createElement('li');
+        liDirector.textContent = 'Director: ' + directors.toString().split(',').join(' ');
+        ul.appendChild(liDirector);
+
+
+        let actors = [];
+
+        movie.majorActors.map(actor => {
+            actors.push([actor.firstName, actor.secondName] + ';');
+        })
+
+        let liActor = document.createElement('li');
+        liActor.textContent = 'Actor: ' + actors.toString().split(',').join(' ');
+        ul.appendChild(liActor);
     });
 }
-
 
 fetch('data/movies.json')
     .then((response) => response.json())
     .then((movies) => {
         init(movies);
     });
-
 
 function init(movies) {
     createDom(movies);
